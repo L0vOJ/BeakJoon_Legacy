663edf42a597f4dfc2f1f741c590d1caed1b6d30
L0vOJ <wonjjw98@gmail.com>
2021-03-29 03:26:42 +0900

    something hhhhhaaaaapppeeend

--- 2812.cpp
+++ 2812.cpp
@@ -3,17 +3,10 @@
 using namespace std;
 int main(){
   string input;
-<<<<<<< HEAD
   int convey[10][50001];
-=======
->>>>>>> d28ad521c0b6a98a64b3f6bb3c2fa794514b21da
   int n,m;
   cin>>n>>m>>input;
   for(int a=0;a<n;a++){
     cout<<input[a];
   }
-<<<<<<< HEAD
-
-=======
->>>>>>> d28ad521c0b6a98a64b3f6bb3c2fa794514b21da
 }
